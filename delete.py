1f25e1991fddc45709cf226864f5d17cb677a1a0
Minh Tu Nguyen <123827805+nguyenm1nhtu@users.noreply.github.com>
2024-10-17 21:45:56 +0700

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,18 +1,15 @@
+//File này được sử dụng nhằm xóa bớt dữ liệu trong các tệp dữ liệu
 import os
 import random
 
 def delete_random_files(folder_path, num_files_to_delete):
-    # Lấy danh sách tất cả các file trong thư mục
     files = os.listdir(folder_path)
     
-    # Chọn ngẫu nhiên số lượng file cần xóa
     files_to_delete = random.sample(files, num_files_to_delete)
     
-    # Xóa các file đã chọn
     for file in files_to_delete:
         file_path = os.path.join(folder_path, file)
         os.remove(file_path)
         print(f"Đã xóa: {file_path}")
 
-# Sử dụng hàm này
-delete_random_files('E:/Homework/Machine_Learning/Human_emotion_dectection/images/train/sad', 328)  # Ví dụ xóa 10 file ngẫu nhiên trong thư mục
+delete_random_files('E:/Homework/Machine_Learning/Human_emotion_dectection/images/train/sad', 328)
